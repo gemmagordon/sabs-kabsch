bd63aada85a58bf13555ae42bd1b00cbdc73f04e
gemma gordon <gemmagordon2012@gmail.com>
2022-02-10 10:57:54 +0000

    package name issues

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 setup(
     name='sabs_kabsch',
-    packages=find_packages(),
+    packages=find_packages(include=['sabs_kabsch']),
     version='0.1.0',
     description='Implementation of the Kabsch algorithm as a Python library',
     author='Gemma Gordon',
     license='MIT',
-    install_requires=['numpy==1.22.1', 'biopython==1.79']
+    install_requires=['biopython==1.79', 'matplotlib==3.5.1', 'numpy==1.22.2']
 )
